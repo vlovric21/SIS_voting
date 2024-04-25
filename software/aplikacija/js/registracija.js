106487c1a9e4954ca7cfcdcd26ea2eda5afb01e6
Magdalena Markovinvić <mmarkovin21@foi.hr>
2024-04-25 22:36:59 +0200

    #12 Dodan p element u koji se ispisuje greška sa sarvera.

--- software/aplikacija/js/registracija.js
+++ software/aplikacija/js/registracija.js
@@ -2,6 +2,7 @@ document.addEventListener("DOMContentLoaded", async () => {
     let username = document.getElementById("username");
     let email = document.getElementById("email");
     let password = document.getElementById("password");
+    let greska = document.getElementById("greska")
     let retryPassword = document.getElementById("retryPassword");
     let form = document.getElementById("registration-form");
 
@@ -25,6 +26,10 @@ document.addEventListener("DOMContentLoaded", async () => {
         if(res.status == 201){
             location.href = "/prijava";
             console.log(location);
+        }else if(res.status == 400){
+            let responseText = (await res.text()).replace(/["{}]/g, " ");
+            greska.style.display = "block";
+            greska.innerHTML = `<p>${responseText}</p>`;
         }
     });
 
