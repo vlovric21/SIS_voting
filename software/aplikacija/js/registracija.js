3da9dcd8cb0cb0c4f071253d99cd8dd1e4f3b2fb
Magdalena Markovinvić <mmarkovin21@foi.hr>
2024-05-02 18:04:28 +0200

    #12 Provjerena ponovno upisana lozinka na registraciji.
    Ispravljen regexs na registraciji.

--- software/aplikacija/js/registracija.js
+++ software/aplikacija/js/registracija.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", async () => {
     let email = document.getElementById("email");
     let password = document.getElementById("password");
     let greska = document.getElementById("greska")
-    let retryPassword = document.getElementById("retryPassword");
+    let retryPassword = document.getElementById("retryPasword");
     let form = document.getElementById("registration-form");
 
     form.addEventListener("submit", async (e) => {
@@ -17,19 +17,23 @@ document.addEventListener("DOMContentLoaded", async () => {
             lozinka: password.value,
             mail: email.value
         }
-
-        let res = await fetch("/api/korisnici", {
-            method: "POST",
-            headers: heder,
-            body: JSON.stringify(body)
-        });
-        if(res.status == 201){
-            location.href = "/prijava";
-            console.log(location);
-        }else if(res.status == 400){
-            let responseText = (await res.text()).replace(/["{}]/g, " ");
+        if(password.value == retryPassword.value){
+            let res = await fetch("/api/korisnici", {
+                method: "POST",
+                headers: heder,
+                body: JSON.stringify(body)
+            });
+            if(res.status == 201){
+                location.href = "/prijava";
+                console.log(location);
+            }else if(res.status == 400){
+                let responseText = (await res.text()).replace(/("|{|}|\bgreska\b|:)/g, " ");
+                greska.style.display = "block";
+                greska.innerHTML = `<p>${responseText}</p>`;
+            }
+        }else{
             greska.style.display = "block";
-            greska.innerHTML = `<p>${responseText}</p>`;
+            greska.innerHTML = `<p>Lozinka i ponvljena lozinka nisu iste!</p>`;
         }
     });
 
