5e8a7c491c157d4590c2323e4ae9d847e020e7bc
Magdalena Markovinvić <mmarkovin21@foi.hr>
2024-05-05 14:23:10 +0200

    #12 Uspješno dohvaćena pitanja i prikazana pitanja.

--- software/aplikacija/js/pocetna.js
+++ software/aplikacija/js/pocetna.js
@@ -1,23 +1,69 @@
 document.addEventListener("DOMContentLoaded", async () => {
-
-    dajJWT();
+    dohvatiPitanja(1);
     
 });
 
 async function dohvatiPitanja(str){
+    let token = await  dajJWT();
 
+    if(token != null){
+        let zaglavlje = new Headers();
+        zaglavlje.set("Content-Type", "application/json");
+        zaglavlje.set("Authorization", `Bearer ${token}`);
+        let parametri = {
+            method: "GET",
+            headers: zaglavlje
+        }
+
+        let odgovor = await fetch(
+            `/api/pitanja?str=`+ str,
+            parametri
+        );
+
+        if (odgovor.status == 200) {
+            let podaci = await odgovor.text();
+            podaci = JSON.parse(podaci);
+
+            prikaziPitanja(podaci);
+        }
+    }
 }
 
-
 async function dajJWT(){
-    let odgovor = await fetch(`/api/korisnici/${sessionStorage.username}/prijava`); // Ide se na GET metodu _/api/korisnici/{korime}/prijava_ kako bi se dobio JWT
+    let odgovor = await fetch(`/api/korisnici/${sessionStorage.username}/prijava`);
     if (odgovor.status != 201) {
         return {};
     }
 
-    let token = odgovor.headers.get("authorization").split(" ")[1]; // Čita JWT token iz zaglavlja odgovora
-    console.log(token);
+    let token = odgovor.headers.get("authorization").split(" ")[1];
     if (token == "" || token == null || token == undefined) {
-        return 0; // Ukoliko nema tokena (ne postoji sesija ili slično)
+        return 0;
+    }else return token;
+}
+
+async function prikaziPitanja(pitanja){
+    let listaPitanja = document.getElementById("lista-pitanja");
+	let lista = '<div id="pitanje" class="kartica-pitanja">';
+    for (p of pitanja){
+        lista += `<div class="autor">Autor: ${p.autor}</div>`;
+        lista += `<h2 class="naslov-pitanja">${p.pitanje}</h2>`
+        lista += `<form id="pitanje1" action="#">
+                    <div id="odgovori1" class="odgovori">
+                    ${prikaziOdgovore(p)}
+                    </div>
+                    <button type="button">Dalje</button>
+                </form>`;
     }
+
+	listaPitanja.innerHTML = lista;
+}
+
+async function prikaziOdgovore(pitanje){
+    let odgovori = document.getElementById("odgovori1");
+    let listaOdg = "";
+    for (o of pitanje){
+        listaOdg += `<input type="radio" id="odgovor1" name="odgovor" value="odgovor1">
+        <label for="odgovor1">Odgovor 1</label><br>`;
+    }
+    odgovori.innerHTML = listaOdg;
 }
