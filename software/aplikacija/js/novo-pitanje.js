9cdecfccdce32d830903a32edb4079f74aed999f
Magdalena Markovinvić <mmarkovin21@foi.hr>
2024-05-06 13:32:32 +0200

    #12 Pitanja se uspješno spremaju u bazu.
    Postoji neki error :
    Uncaught (in promise) DOMException: The operation was aborted.

--- software/aplikacija/js/novo-pitanje.js
+++ software/aplikacija/js/novo-pitanje.js
@@ -1,19 +1,86 @@
 document.addEventListener("DOMContentLoaded", async () => {
-    let jednostrukiGumb = document.getElementById("jednostruki-gumb");
-    let visestrukiGumb = document.getElementById("visestruki-gumb");
+    let form = document.getElementById("formaNovoPitanje");
 
-    jednostrukiGumb.addEventListener("click", () =>{
-        jednostrukiOdgovori();
+    jednostrukiOdgovori();
+
+
+    form.addEventListener("submit", async (event) =>{
+        event.preventDefault();
+
+        await posaljiNovoPitanje();
     });
-    
-
 });
+
+async function dajJWT(){
+    let odgovor = await fetch(`/api/korisnici/${sessionStorage.username}/prijava`);
+    if (odgovor.status != 201) {
+        return {};
+    }
+    let token = odgovor.headers.get("authorization").split(" ")[1];
+    if (token == "" || token == null || token == undefined) {
+        return 0;
+    }else return token;
+}
+
+async function posaljiNovoPitanje(){
+    let token = await dajJWT();
+    let naslovPitanja = document.getElementById("pitanje-input");
+
+    let tijelo ={
+        pitanje: naslovPitanja.value,
+        odabiri: await generirajOdgovoreZaSlanje()
+    }
+    console.log(tijelo);
+
+    if(token != null){
+        let zaglavlje = new Headers();
+        zaglavlje.set("Content-Type", "application/json");
+        zaglavlje.set("Authorization", `Bearer ${token}`);
+        let parametri = {
+            method: "POST",
+            headers: zaglavlje,
+            body: JSON.stringify(tijelo)
+        }
+
+        let odgovor = await fetch(
+            `/api/pitanja`,
+            parametri
+        );
+
+        if (odgovor.status == 201) {
+            location.href = "/pocetna";
+            let responseText = (await odgovor.text()).replace(/("|{|}|\bgreska\b|:)/g, " ");
+            console.log(responseText);
+        }else{
+            let responseText = (await odgovor.text()).replace(/("|{|}|\bgreska\b|:)/g, " ");
+            console.log(responseText);
+        }
+    }
+}
+
+async function generirajOdgovoreZaSlanje(){
+    let opcije = document.getElementsByClassName("jedna-opcija");
+    let poljeOdabira = [];
+
+    try {
+        await Array.from(opcije).forEach(async (opcija) => {
+            let jedanOdabir = {
+                tekst: opcija.children[0].value
+            };
+            poljeOdabira.push(jedanOdabir);
+        });
+        return poljeOdabira;
+    } catch (error) {
+        console.error('Error in generirajOdgovoreZaSlanje:', error);
+        return []; 
+    }
+}
 
 function jednostrukiOdgovori(){
     let odgovori = document.getElementById("odgovori");
     let vrijednosti = [];
 
-    odgovori.innerHTML = "<h2>Dodaj jednostruki izbor</h2>";
+    odgovori.innerHTML = "<h2>Dodaj izbor</h2>";
 
     odgovori.innerHTML += "<input id='opcijaInput' class='dodaj-izbor' type='text' placeholder ='Opcija'>";
     odgovori.innerHTML += "<button id='addButton' type='button' class='vrsta-odgovora'>+</button><br>";
