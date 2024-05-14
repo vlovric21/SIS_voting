d85fc4c9a50b09a4f75fb1c606fc75b2fa114387
Magdalena Markovinvić <mmarkovin21@foi.hr>
2024-05-14 11:47:05 +0200

    #12 Napravljene regex provjere za registraciju

--- software/aplikacija/js/registracija.js
+++ software/aplikacija/js/registracija.js
@@ -12,34 +12,75 @@ document.addEventListener("DOMContentLoaded", async () => {
             grecaptcha.execute('6Ldl7NgpAAAAAILzx0tyDFwCHgSK_Lazg-nyBhOI', {action: 'submit'}).then(function(token){
                 let heder = new Headers();
                 heder.set("Content-Type", "application/json");
+
+                if(provjeriRegexEmail(email.value) && provjeriRegexUsername(username.value) && provjeriRegexPassword(password.value)){
         
-                let body = {
-                    korime: username.value,
-                    lozinka: password.value,
-                    mail: email.value,
-                    token: token
-                }
-                if(password.value == retryPassword.value){
-                    (async () => {
-                        let res = await fetch("/api/korisnici", {
-                            method: "POST",
-                            headers: heder,
-                            body: JSON.stringify(body)
-                        });
-                        if(res.status == 201){
-                            location.href = "/prijava";
-                            console.log(location);
-                        }else if(res.status == 400){
-                            let responseText = (await res.text()).replace(/("|{|}|\bgreska\b|:)/g, " ");
-                            greska.style.display = "block";
-                            greska.innerHTML = `<p>${responseText}</p>`;
-                        }
-                    })();
-                }else{
-                    greska.style.display = "block";
-                    greska.innerHTML = `<p>Lozinka i ponvljena lozinka nisu iste!</p>`;
+                    let body = {
+                        korime: username.value,
+                        lozinka: password.value,
+                        mail: email.value,
+                        token: token
+                    }
+                    
+                    if(password.value == retryPassword.value){
+                        (async () => {
+                            let res = await fetch("/api/korisnici", {
+                                method: "POST",
+                                headers: heder,
+                                body: JSON.stringify(body)
+                            });
+                            if(res.status == 201){
+                                location.href = "/prijava";
+                                console.log(location);
+                            }else if(res.status == 400){
+                                let responseText = (await res.text()).replace(/("|{|}|\bgreska\b|:)/g, " ");
+                                greska.style.display = "block";
+                                greska.innerHTML = `<p>${responseText}</p>`;
+                            }
+                        })();
+                    }else{
+                        greska.style.display = "block";
+                        greska.innerHTML = `<p>Lozinka i ponvljena lozinka nisu iste!</p>`;
+                    }
                 }
             });
         });
     });
 });
+
+function provjeriRegexUsername(username){
+
+    let regexUsername = /^(?=.*\d).{6,}$/;
+    if(regexUsername.test(username)){
+        return true;
+    }else{
+        greska.style.display = "block";
+        greska.innerHTML = `<p>Korisničko ime mora imati minimalno 6 znakova!</p>`;
+        return false;
+    }
+}
+
+function provjeriRegexEmail(email){
+
+    let regexEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if(regexEmail.test(email)){
+        return true;
+    }else{
+        greska.style.display = "block";
+        greska.innerHTML = `<p>Neispravno upisan email!</p>`;
+        greska.innerHTML += `<p>Podržani oblik je xxx@xxx.xxx</p>`;
+        return false;
+    }
+}
+
+function provjeriRegexPassword(password){
+
+    var regexPassword = /^(?=.*\d)(?=.*[!@#$%^&*()-_+=])[^\s]{8,}$/;
+    if(regexPassword.test(password)){
+        return true;
+    }else{
+        greska.style.display = "block";
+        greska.innerHTML = `<p>Lozinka mora sadržavati minimalno 8 znakova od kojih je jedan broj i jedan poebni znak ($, #, !, ?, ^)!</p>`;
+        return false;
+    }
+}
